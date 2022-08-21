8f952a4395120bd8e12239641beb5663e59e80d6
serifselim <selimserifoglu03@gmail.com>
2022-08-21 22:39:02 +0300

    css

--- src/components/visualComponents/Header/Header.vue
+++ src/components/visualComponents/Header/Header.vue
@@ -1,35 +1,17 @@
 <template>
-  <header class="w-full">
-    <section class="w-full bg-red p-10 flex justify-between">
+  <header class="header">
+    <section class="header__content flex align-center justify-between">
       <div>
         <Logo />
       </div>
       <div class="flex items-center">
-        <ul class="flex mr-6 gap-6">
-          <li class="hover:text-green text-lg cursor-pointer">
-            [<span class="text-green">0</span>]About
-          </li>
-          <li class="hover:text-green text-lg cursor-pointer">
-            [<span class="text-green">1</span>]Projects
-          </li>
-          <li class="hover:text-green text-lg cursor-pointer">
-            [<span class="text-green">2</span>]Source
-          </li>
+        <ul class="flex nav">
+          <li class="nav__item text-size">[<span>0</span>]About</li>
+          <li class="nav__item text-size">[<span>1</span>]Projects</li>
+          <li class="nav__item text-size">[<span>2</span>]Source</li>
         </ul>
         <div>
-          <button
-            class="
-              font-medium
-              rounded
-              transition-colors
-              border-[3px] border-green
-              px-5
-              py-1
-              hover:bg-green hover:text-iron
-            "
-          >
-            Resume
-          </button>
+          <button class="resume text-size">Resume</button>
         </div>
       </div>
     </section>
@@ -43,3 +25,53 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.header {
+  width: 100%;
+}
+
+.header__content {
+  width: 100%;
+  padding: 2.4rem;
+}
+
+.nav {
+  margin-right: 2rem;
+}
+
+.nav__item {
+  cursor: pointer;
+  margin-block: auto;
+}
+
+.nav__item:not(:first-child) {
+  margin-left: 2rem;
+}
+
+.nav__item:hover {
+  color: var(--primary-color);
+}
+
+.nav__item span {
+  color: var(--primary-color);
+}
+
+.resume {
+  border-radius: 1.2rem;
+  transition: background 0.3s;
+  border: 3px solid var(--primary-color);
+  padding: 0.9rem 2rem;
+  background: transparent;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  font-weight: 600;
+  justify-content: center;
+}
+
+.resume:hover {
+  background: var(--primary-color);
+  color: var(--negative-text-color);
+}
+</style>
