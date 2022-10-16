79fd9057c18e4f16865178d95f5475128c80c469
serifselim <selimserifoglu03@gmail.com>
2022-10-16 13:06:07 +0300

    toast was added to the project

--- src/components/Footer/FooterRight.vue
+++ src/components/Footer/FooterRight.vue
@@ -1,18 +1,40 @@
 <template>
-    <div class="fixed right-11 bottom-0 md:visible animate__fadeInUpBig">
-        <span class="text-vertical mr-5 text-center">yavuzserifoglu@gmail.com</span>
-        <div class="w-1 h-20 bg-dark mx-auto mt-2" />
-    </div>
+  <div class="fixed right-11 bottom-0 md:visible animate__fadeInUpBig">
+    <span
+      @click="copyEmail"
+      class="text-vertical hover:text-primary text-center cursor-pointer"
+      >yavuzserifoglu@gmail.com</span
+    >
+    <div class="w-1 h-20 bg-dark ml-2 mx-auto mt-2" />
+    <toast-vue v-if="isCopied" />
+  </div>
 </template>
 
 <script>
+import ToastVue from "../Global/Toast.vue";
 export default {
-    name: "FooterRight"
-}
+  name: "FooterRight",
+  data: () => ({
+    isCopied: false,
+  }),
+  methods: {
+    copyEmail() {
+      navigator.clipboard.writeText("yavuzserifoglu@gmail.com");
+      this.isCopied = true;
+      setTimeout(() => {
+        this.isCopied = false;
+      }, 1000);
+    },
+  },
+
+  components: {
+    ToastVue,
+  },
+};
 </script>    
 
 <style scoped>
 .text-vertical {
-    writing-mode: vertical-lr;
+  writing-mode: vertical-lr;
 }
 </style>
